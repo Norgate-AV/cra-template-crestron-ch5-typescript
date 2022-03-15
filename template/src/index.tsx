55f57f692fea5d0917ebc9eee3c54781a91ef44b
Damien Butt <damien.butt@norgateav.com>
2022-03-15 19:38:07 +0000

    feat(package): add crestron event bridges to index.tsx

--- template/src/index.tsx
+++ template/src/index.tsx
@@ -1,11 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import {
+	bridgeReceiveIntegerFromNative,
+	bridgeReceiveBooleanFromNative,
+	bridgeReceiveStringFromNative,
+	bridgeReceiveObjectFromNative,
+} from "@crestron/ch5-crcomlib";
 import App from "./App";
 
+(window as any).bridgeReceiveIntegerFromNative = bridgeReceiveIntegerFromNative;
+(window as any).bridgeReceiveBooleanFromNative = bridgeReceiveBooleanFromNative;
+(window as any).bridgeReceiveStringFromNative = bridgeReceiveStringFromNative;
+(window as any).bridgeReceiveObjectFromNative = bridgeReceiveObjectFromNative;
+
 ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById("root")
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById("root"),
 );
