36756d9b159e24bb2c6d530bb491d50658cf1ae8
Damien Butt <damien.butt@norgateav.com>
2022-03-30 10:32:15 +0100

    feat(ch5-logo): add ch5-logo markup and styles

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import reactLogo from "./logo.svg";
+import ch5Logo from "./assets/crestron-ch5-logo.png";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<div className="logo-container">
+					<img
+						src={reactLogo}
+						className="react-logo"
+						alt="react-logo"
+					/>
+					<p>+</p>
+					<div className="ch5-logo">
+						<img src={ch5Logo} alt="crestron-ch5-logo" />
+					</div>
+				</div>
+				<p>
+					Edit <code>src/App.tsx</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+			</header>
+		</div>
+	);
 }
 
 export default App;
