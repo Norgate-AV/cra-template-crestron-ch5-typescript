c2b0700fdda7b93fc85e71109abf978a07f9b48e
Damien Butt <damien.butt@norgateav.com>
2022-04-04 19:05:43 +0100

    feat(index.tsx): update react-dom usage for react v18 (#16)

--- template/src/index.tsx
+++ template/src/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import { HashRouter as Router } from "react-router-dom";
 import "./index.css";
 import {
@@ -22,11 +22,11 @@ if (isCrestronTouchscreen()) {
 		bridgeReceiveObjectFromNative;
 }
 
-ReactDOM.render(
+const container = document.getElementById("root") as HTMLElement;
+ReactDOM.createRoot(container).render(
 	<React.StrictMode>
 		<Router>
 			<App />
 		</Router>
 	</React.StrictMode>,
-	document.getElementById("root"),
 );
