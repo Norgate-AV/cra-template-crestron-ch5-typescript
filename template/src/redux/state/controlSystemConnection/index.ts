90deb9d08c7898df54c8de1847908dc61cd3a0b0
Damien Butt <damien.butt@norgateav.com>
2022-04-10 19:10:58 +0100

    feat(redux): add initial setup boilerplate

---
+++ template/src/redux/state/controlSystemConnection/index.ts
@@ -0,0 +1,35 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import type { RootState } from "../../store";
+
+export interface IControlSystemConnection {
+	online: boolean;
+}
+
+const initialState: IControlSystemConnection = {
+	online: false,
+};
+
+export const controlSystemConnectionSlice = createSlice({
+	name: "controlSystemConnection",
+	initialState,
+	reducers: {
+		setControlSystemOnline: (
+			state: IControlSystemConnection,
+			action: PayloadAction<boolean>,
+		) => {
+			state.online = action.payload;
+			console.log(
+				`Control System ${state.online ? "Online" : "Offline"}`,
+			);
+		},
+	},
+});
+
+export const { setControlSystemOnline } = controlSystemConnectionSlice.actions;
+
+export const selectControlSystemConnection = (state: RootState) =>
+	state.controlSystemConnection;
+export const selectControlSystemOnline = (state: RootState) =>
+	state.controlSystemConnection.online;
+
+export default controlSystemConnectionSlice.reducer;
