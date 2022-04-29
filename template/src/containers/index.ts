4570a5d8b62a4c652204588f27d1fe5fc58a6e33
Damien Butt <damien.butt@norgateav.com>
2022-04-29 14:47:05 +0100

    feat: add containers directory

---
+++ template/src/containers/index.ts
@@ -0,0 +1,21 @@
+/**
+ * Place any containers in this directory.
+ * Ideally use a subdirectory for each.
+ *
+ * For example:
+ *
+ * containers/
+ * |-> index.ts
+ * |-> SomeContainer/
+ *     |-> index.tsx
+ *
+ * Then export the container from this file:
+ * export { SomeContainer } from "./SomeContainer";
+ * or
+ * export * from "./SomeContainer";
+ *
+ * Then import the container from the containers directory:
+ * import { SomeContainer } from "../containers";
+ */
+
+export {};
