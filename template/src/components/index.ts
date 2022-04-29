8a2e523e92c967128842258c7ea62a533ca0325f
Damien Butt <damien.butt@norgateav.com>
2022-04-29 14:47:37 +0100

    feat: add components directory

---
+++ template/src/components/index.ts
@@ -0,0 +1,21 @@
+/**
+ * Place any components in this directory.
+ * Ideally use a subdirectory for each.
+ *
+ * For example:
+ *
+ * components/
+ * |-> index.ts
+ * |-> SomeComponent/
+ *     |-> index.tsx
+ *
+ * Then export the component from this file:
+ * export { SomeComponent } from "./SomeComponent";
+ * or
+ * export * from "./SomeComponent";
+ *
+ * Then import the component from the components directory:
+ * import { SomeComponent } from "../components";
+ */
+
+export {};
