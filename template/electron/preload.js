51ee5f78d078fb48930c42334864fbb7b124c776
Damien Butt <damien.butt@norgateav.com>
2022-05-02 16:34:32 +0100

    feat: add electron support (#35)
    * chore: add electron packages and deps
    
    * chore: add electron scripts
    
    * chore: add main property for electron
    
    * chore: add electron setup files and boilerplate
    
    * chore: bump electron packages to latest
    
    * refactor: do not show dev tools by default
    
    * chore: include electron directory in eslint tsconfig

---
+++ template/electron/preload.js
@@ -0,0 +1,7 @@
+// preload.js
+
+// All of the Node.js APIs are available in the preload process.
+// It has the same sandbox as a Chrome extension.
+window.addEventListener("DOMContentLoaded", () => {
+    console.log("Electron Preload: DOMContentLoaded");
+});
