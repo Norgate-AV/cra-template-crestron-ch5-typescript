2b7a482e13794c49ccc31097044ab12fa46b1281
Damien Butt <damien.butt@norgateav.com>
2022-05-02 22:12:51 +0100

    feat(tailwindcss): add dracula theme colour palette as custom classes

--- template/tailwind.config.js
+++ template/tailwind.config.js
@@ -1,7 +1,21 @@
 module.exports = {
     content: ["./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                "dracula-background": "#22212C",
+                "dracula-comment": "#7970A9",
+                "dracula-selection": "#454158;",
+                "dracula-foreground": "#F8F8F2",
+                "dracula-cyan": "#80FFEA",
+                "dracula-green": "#8AFF80",
+                "dracula-orange": "#FFCA80",
+                "dracula-pink": "#FF80BF",
+                "dracula-purple": "#9580FF",
+                "dracula-red": "#FF9580",
+                "dracula-yellow": "#FFFF80",
+            },
+        },
     },
     plugins: [],
 };
