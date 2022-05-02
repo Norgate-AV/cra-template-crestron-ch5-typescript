3a29d8d8e82cbbefa3ae8bb528bebaf22b334b82
Damien Butt <damien.butt@norgateav.com>
2022-05-02 22:08:05 +0100

    chore: update comments on containers folder structure

--- template/src/containers/index.ts
+++ template/src/containers/index.ts
@@ -6,30 +6,33 @@
  *
  * containers/
  * |-> SomeContainer/
- * |   |-> index.ts
+ * |   |-> index.ts (export * from "./SomeContainer";)
  * |   |-> SomeContainer.styles.ts
  * |   |-> SomeContainer.tsx
  * |-> pages/
  * |   |-> Page/
- * |   |   |-> index.ts
+ * |   |   |-> index.ts (export * from "./Page";)
  * |   |   |-> Page.styles.ts
  * |   |   |-> Page.tsx
  * |   |   |-> Page.test.tsx
  * |   |-> AnotherPage/
- * |   |   |-> index.ts
+ * |   |   |-> index.ts (export * from "./AnotherPage";)
  * |   |   |-> AnotherPage.styles.ts
  * |   |   |-> AnotherPage.tsx
  * |   |   |-> AnotherPage.test.tsx
- * |   |-> index.ts
+ * |   |-> index.ts (export * from "./Page";)
+ * |                (export * from "./AnotherPage";)
  * |-> index.ts
  *
  * Then export the container from this file:
  * export { SomeContainer } from "./SomeContainer";
+ * export { Page, AnotherPage } from "./pages";
  * or
  * export * from "./SomeContainer";
+ * export * from "./pages";
  *
- * Then import the container from the containers directory:
- * import { SomeContainer } from "../containers";
+ * Then import the container(s) from the containers directory:
+ * import { SomeContainer, Page, AnotherPage } from "../containers";
  */
 
 export {};
